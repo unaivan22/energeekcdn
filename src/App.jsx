b78e6651f69c5f1c4d16c8d8d91813f3003f79aa
unaivan <unaivan22@gmail.com>
2025-05-21 10:09:09 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,51 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { DialogDemo } from './Dialogdemo'
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "~/components/ui/table"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const links = [
+    {
+      desc: "DSDABM Masonry Gallery",
+      url: "/dsdabmgallerymasonry"
+    }
+  ]
+
 
   return (
     <>
-      <DialogDemo />
+      <div className='grid min-h-screen place-items-center'>
+        <div className='container max-w-4xl'>
+          <Table>
+            <TableCaption>A list of url</TableCaption>
+            <TableHeader>
+              <TableRow>
+                <TableHead className='text-center'>Deskripsi</TableHead>
+                <TableHead className='text-center'>URL</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {links.map((link) => (
+                <TableRow key={link.desc}>
+                  <TableCell>{link.desc}</TableCell>
+                  <TableCell>
+                    <a href={link.url} /> {link.url}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+      </div>
     </>
   )
 }
